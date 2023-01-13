2192d704ebcf9250203e45091469f7553959fa7c
manzoranaqibullah <manzoranaqibullah@localhost>
2023-01-12 19:12:31 -0500

    new version

--- src/commit/push/Pushing.java
+++ src/commit/push/Pushing.java
@@ -5,6 +5,8 @@ public class Pushing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("push the class");
+		
+		System.out.println("push the code ");
 	}
 
 }
